7e751d3e28e0a7945a0244c3776be57c1195b901
Shrey354 <71603792+Shrey354@users.noreply.github.com>
2026-06-17 22:05:10 +0545

    Update Quiz_game.py

--- Quiz_game.py
+++ Quiz_game.py
@@ -5,7 +5,7 @@ def game():
     query = input("Do you want to play the game? (Yes/No) : ").lower()
     if query == "yes":
         print("Start Playing")
-        question1 = input("What is capital city of Nepal: ")
+        question1 = input("What is capital city of Nepal: ").lower()
         if question1 == "Kathmandu" :
             print("Correct Answer")
             score +=1
@@ -17,13 +17,13 @@ def game():
             score +=1
         else:
             print("Incorrect Answer")
-        question3 = input("What is full form of CPU: ")
+        question3 = input("What is full form of CPU: ").lower()
         if question3 == "Central Processing Unit" :
             print("Correct Answer")
             score +=1
         else:
             print("Incorrect Answer")
-        question4 = input("What is full form of GPU: ")
+        question4 = input("What is full form of GPU: ").lower()
         if question4 == "Graphical Processing Unit" :
             print("Correct Answer")
             score +=1
